4bebaffed281fd15d76d6053a928d28a63bee812
Malte Kiefer <malte.kiefer@mailbox.org>
2026-04-11 18:19:31 +0200

    chore: gradle root + version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "ProxMoxOpen"
+include(":app", ":domain",
+        ":data:api", ":data:db", ":data:secrets",
+        ":core:ui", ":core:common")
