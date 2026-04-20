5532200bbe7b461a02972b17e8d889500afc03f3
Malte Kiefer <malte.kiefer@mailbox.org>
2026-04-20 22:08:55 +0200

    fix(db): use System.loadLibrary for sqlcipher-android 4.6
    loadLibs was removed in the new sqlcipher-android package; load the
    native library directly in Application.onCreate instead.

--- app/src/main/kotlin/de/kiefer_networks/proxmoxopen/ProxMoxOpenApp.kt
+++ app/src/main/kotlin/de/kiefer_networks/proxmoxopen/ProxMoxOpenApp.kt
@@ -2,14 +2,13 @@ package de.kiefer_networks.proxmoxopen
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import net.zetetic.database.sqlcipher.SQLiteDatabase
 import timber.log.Timber
 
 @HiltAndroidApp
 class ProxMoxOpenApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        SQLiteDatabase.loadLibs(this)
+        System.loadLibrary("sqlcipher")
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
